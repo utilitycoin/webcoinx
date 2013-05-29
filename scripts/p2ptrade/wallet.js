e9c8bc3df795bec2d740025fd16fdf44a6b07683
Karel Tuma <karel.tuma@gmail.com>
2013-05-29 23:29:20 +0200

    Implement sign-as-we-go, sending change back, distribute out sigscripts to wire exchange.
    Signed-off-by: Karel Tuma <karel.tuma@gmail.com>

--- scripts/p2ptrade/wallet.js
+++ scripts/p2ptrade/wallet.js
@@ -12,15 +12,17 @@ define(["jquery"], function($) {
          return text;
      }
 
+	 // returns [ "txhash:id" , "txhash:id" ... ]
      MockExchangeTransaction.prototype.collectMyOutpoints = function(colorid) {
         
      };
 
 
-     function MockExchangeTransaction (wallet, data) {
+     function MockExchangeTransaction(wallet, data) {
          this.wallet = wallet;
          this.tx = data.tx;
          this.my = data.my;
+		 this.realtx = null;
      }
      MockExchangeTransaction.prototype.fetchOutputColors = function (next) {
          // TODO: use colorman on all outputs we don't know, and after colors are known
@@ -35,20 +37,59 @@ define(["jquery"], function($) {
                                  });
          return (total >= value);
      };
-     MockExchangeTransaction.prototype.convert = function(first_argument) {
-         var tx = new BitcoinTransaction();
+
+	 	
+	 // reconstruct tx so bitcoinjs-lib understands it
+     MockExchangeTransaction.prototype.getRealTx = function () {
+		 // cache
+		 if (this.realtx) return this.realtx;
+
+         var realtx = new Bitcoin.Transaction();
+
+		 // inputs
          this.tx.inp.forEach(function(inp) {
-            tx.ins.push(new TransactionIn({
+            realtx.ins.push(new TransactionIn({
                 outpoint: inp.outpoint,
                 script: script: new Bitcoin.Script(),
                 sequence: 4294967295
-            });
+            }));
          });
-     };
+
+		 // outputs
+		 this.tx.out.forEach(function(out) {
+			 realtx.outs.push(new TransactionOut({
+				 value: BigInteger.valueOf(out.value), // XXX fp
+				 script: Script.createOutputScript(address)
+			 }));
+		 });
+		 this.realtx = realtx;
+
+		 return realtx;
+	}
+
+
      MockExchangeTransaction.prototype.signMyInputs = function (reftx) {
          var my = reftx ? reftx.my : this.my;
+
+		 var real = this.getRealTx();
+
+		 for (var i = 0; i < this.tx.inp.length; i++) {
+			 var inp = this.tx.inp[i];
+			 if (my.indexOf(inp.outpoint) < 0) continue;
+			 var utxo = real.ins[i].utxo;
+			 if (!utxo)
+				 throw "missing utxo for outpoint";
+			 var hash = real.hashTransactionForSignature(utxo.out.script, i, 1); // SIGHASH_ALL
+			 var pkhash = utxo.out.script.simpleOutPubKeyHash();
+			 var sig = this.signWithKey(pkhash, hash);
+			 sig.push(parseInt(hashType, 10));
+			 var pk = real.getPubKeyFromHash(pkhash);
+
+			 // this will be done somewhere else when real is reconstructed to be dispatched off to network 
+//			 real.ins[i].script = Script.createInputScript(sig, pk);
+			 inp.signed = { sig: sig, pk: pk };
+		 }
          this.tx.inp.forEach(function (inp) {
-                                if (my.indexOf(inp.outpoint) >= 0)
                                     inp.signed = true;
                                  });
          return true;
@@ -72,6 +113,8 @@ define(["jquery"], function($) {
          this.tx.inp = this.tx.inp.concat(etx.tx.inp);
          this.tx.out = this.tx.out.concat(etx.tx.out);
          this.my = this.my.concat(etx.my);
+		 // invalidate realtx cache
+		 this.realtx = null;
      };
      MockExchangeTransaction.prototype.getData = function () {
          return this.tx;
@@ -92,13 +135,19 @@ define(["jquery"], function($) {
 			 return this.wallet.getCurAddress().toString();
      };
      MockWallet.prototype.createPayment = function (color, amount, to_address) {
-         var outpoints = this.collectMyOutpoints(color);
-         return new MockExchangeTransaction(this, 
+         var utxos = this.collectMyUTXO(color, amount);
+         var t = MockExchangeTransaction(this, 
                                             {
-                                                tx: {inp: [outpoints],
+                                                tx: {inp: utxos.list.forEach.function(utxo) {return { outpoint: utxo.outpoint, utxo: utxo, signed: false};}},
                                                      out: [{to: to_address, value: amount, color: color}]},
                                                 my: outpoints
                                             });
+		 // if the total is larger, append sending change back
+		 if (utxos.total > amount) {
+			 t.tx.out.push({to: this.getAddress(), value: utxos.total - amount});
+			 t.realtx = null;
+		 }
+		 return t;
      };
      MockWallet.prototype.importTx = function (tx_data) {
          return new MockExchangeTransaction(this, 
