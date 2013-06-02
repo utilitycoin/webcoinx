f7af543ea5ea47c5c67c685e3d5489a158007e66
Alex Mizrahi <alex.mizrahi@gmail.com>
2013-06-02 22:13:04 +0300

    two p2ptrade agents for easy testing

--- scripts/p2ptrade/gui.js
+++ scripts/p2ptrade/gui.js
@@ -1,16 +1,18 @@
 define(["jquery", "p2ptrade/comm", "p2ptrade/agent", "p2ptrade/offer", "p2ptrade/wallet"], function($, HTTPExchangeComm, ExchangePeerAgent, ExchangeOffer, MockWallet) {
 
-        var comm = null;
-        var epa = null;
+        var comm1 = null;
+        var epa1 = null;
+        var comm2 = null;
+        var epa2 = null;
 
         var color1 = "1111";
         var color2 = "2222";
 
         function updateGUIstate() {
-            var active = epa.hasActiveEP();
+            var active = epa1.hasActiveEP();
             var text = "";
             if (active) {
-                text = "Transaction in progress: " + epa.active_ep.state;
+                text = "Transaction in progress: " + epa1.active_ep.state;
 
             }
 
@@ -18,18 +20,24 @@ define(["jquery", "p2ptrade/comm", "p2ptrade/agent", "p2ptrade/offer", "p2ptrade
         }
 
         function p2pgui(wm, cm, exit) {
-            comm = new HTTPExchangeComm('http://webcoinx.tumak.cz/messages');
-            epa = new ExchangePeerAgent(new MockWallet(wm, cm, exit), comm);
-            comm.addAgent(epa);
+            comm1 = new HTTPExchangeComm('http://webcoinx.tumak.cz/messages');
+            epa1 = new ExchangePeerAgent(new MockWallet(wm, cm, exit), comm1);
+            comm1.addAgent(epa1);
+
+            comm2 = new HTTPExchangeComm('http://webcoinx.tumak.cz/messages');
+            epa2 = new ExchangePeerAgent(new MockWallet(wm, cm, exit), comm2);
+            comm2.addAgent(epa2);
+
             window.setInterval(function() {
-                    comm.update();
+                    comm1.update();
+                    comm2.update();
                     updateGUIstate();
                 }, 2000);
             var self = this;
 
             $('#buy-button').click(function(event) {
                     event.preventDefault();
-                    epa.registerMyOffer(
+                    epa1.registerMyOffer(
                         new ExchangeOffer(null, {
                                 colorid: false,
                                 value: 11
@@ -41,7 +49,7 @@ define(["jquery", "p2ptrade/comm", "p2ptrade/agent", "p2ptrade/offer", "p2ptrade
 
             $('#sell-button').click(function() {
                     event.preventDefault();
-                    epa.registerMyOffer(
+                    epa2.registerMyOffer(
                         new ExchangeOffer(null, {
                                 colorid: self.colorid,
                                 value: 22
@@ -54,7 +62,7 @@ define(["jquery", "p2ptrade/comm", "p2ptrade/agent", "p2ptrade/offer", "p2ptrade
 
         p2pgui.prototype.setCurrentColor = function(colorid) {
         	this.colorid = colorid;
-        }
+        };
 
         return p2pgui;
 
